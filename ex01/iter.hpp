df8a5c6b4fd843f4d3d620a33cb1c5df5b53dc3c
Joel Lehtonen <jlehtone@c3r3p4.hive.fi>
2025-02-20 16:55:51 +0200

    more tests on ex01

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -2,7 +2,8 @@
 #include <string>
 #include <iostream>
 
-template <typename T>void iter(T* array, size_t len, void (*function)(T& param))
+template <typename T>
+void iter(T* array, size_t len, void (*function)(T& param))
 {
 	for (size_t i = 0; i < len; i++)
 	{
