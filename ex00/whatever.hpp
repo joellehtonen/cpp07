017b5b5eea32c5579fca60e31d68827641c97f69
Joel Lehtonen <jlehtone@c1r6p10.hive.fi>
2025-02-26 10:36:36 +0200

    changed ex00 return type to const ref

--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -2,7 +2,8 @@
 #include <string>
 #include <iostream>
 
-template <typename T>void swap(T& a, T& b)
+template <typename T>
+void swap(T& a, T& b)
 {
 	T temp;
 
@@ -11,12 +12,14 @@ template <typename T>void swap(T& a, T& b)
 	b = temp;
 }
 
-template <typename T>T min(T a, T b)
+template <typename T>
+const T& min(const T& a, const T& b)
 {
 	return (a < b) ? a : b;
 }
 
-template <typename T>T max(T a, T b)
+template <typename T>
+const T& max(const T& a, const T& b)
 {
 	return (a > b) ? a : b;
 }
